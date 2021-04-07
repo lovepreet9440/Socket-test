d7e9b7031ce26c8108fb5b07535fbcd7bd8b43c0
lovepreet9440 <lovepreetkaler1999@gmail.com>
2021-04-07 17:45:56 +0530

    initial code

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require("express"),
+  app = express(),
+  server = require("http").createServer(app),
+  io = require("socket.io").listen(server),
+  users = [];
+
+//specify the html we will use
+app.use("/", express.static(__dirname + "/www"));
+
+server.listen(process.env.PORT || 4000);
+
+console.log(`Server started on port ${process.env.PORT || 4000}`);
+
+io.sockets.on("connection", function (socket) {
+  //new user login
+  socket.on("login", function (nickname) {
+    if (users.indexOf(nickname) > -1) {
+      socket.emit("nameExisted");
+    } else {
+      //socket.userIndex = users.length;
+      socket.nickname = nickname;
+      users.push(nickname);
+      socket.emit("loginSuccess");
+      io.sockets.emit("system", nickname, users.length, "login");
+    }
+  });
+  //user leaves
+  socket.on("disconnect", function () {
+    if (socket.nickname != null) {
+      //users.splice(socket.userIndex, 1);
+      users.splice(users.indexOf(socket.nickname), 1);
+      socket.broadcast.emit("system", socket.nickname, users.length, "logout");
+    }
+  });
+  //new message get
+  socket.on("postMsg", function (msg, color) {
+    socket.broadcast.emit("newMsg", socket.nickname, msg, color);
+  });
+  //new image get
+  // socket.on("img", function (imgData, color) {
+  //   socket.broadcast.emit("newImg", socket.nickname, imgData, color);
+  // });
+});
